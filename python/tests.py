531d343a3b1f009d45f2e5cf538496d412e4fd3c
Elan van Biljon <elan@vanbiljon.co.za>
2017-08-08 12:05:25 +0200

    Add basic tests for Moveable constructor

--- python/tests.py
+++ python/tests.py
@@ -1,5 +1,6 @@
 import unittest
 from coord import Coord
+from moveable import Moveable
 
 class CoordTestCase(unittest.TestCase):
 	def setUp(self):
@@ -54,6 +55,36 @@ class CoordTestCase(unittest.TestCase):
 		self.assertEqual(dist, Coord(x=0, y=0).distance(Coord(x=0, y=-2)))
 		self.assertEqual(dist, Coord(x=0, y=0).distance(Coord(x=2, y=0)))
 		self.assertEqual(dist, Coord(x=0, y=0).distance(Coord(x=-2, y=0)))
+
+class MoveableTestCase(unittest.TestCase):
+	def setUp(self):
+		self.test_x = 1
+		self.test_y = 2
+		self.test_range = 3.0
+		self.mover_a = Moveable(self.test_x, self.test_y)
+		self.mover_b = Moveable(self.test_x, self.test_y, self.test_range)
+
+	def tearDown(self):
+		del self.mover_a
+		del self.mover_b
+
+	def test_constructor(self):
+		with self.assertRaises(TypeError):
+			Moveable((1, 2))
+
+		with self.assertRaises(TypeError):
+			Moveable(1, "asdf", max_dist=1.0)
+
+		with self.assertRaises(TypeError):
+			Moveable(1, 2, max_dist="asdf")
+
+	def test_range(self):
+		self.assertEqual(self.test_range, self.mover_b.range)
+		self.assertNotEqual(self.test_range-1, self.mover_b.range)
+
+	def test_coord(self):
+		self.assertEqual(self.test_x, self.mover_a.x)
+		self.assertEqual(self.test_y, self.mover_a.y)
 
 if __name__ == "__main__":
     unittest.main()
