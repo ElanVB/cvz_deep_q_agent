fb408bc99f565a36cf1f892842733d573f0d018a
Elan van Biljon <elan@vanbiljon.co.za>
2017-08-08 12:06:17 +0200

    Add constructor for Moveable

--- python/moveable.py
+++ python/moveable.py
@@ -1,5 +1,11 @@
 from coord import Coord
 
 class Moveable(Coord):
-	__init__(self, position, max_dist=None):
-		pass
+	def __init__(self, x, y, max_dist=None):
+		if max_dist != None:
+			if not isinstance(max_dist, (int, float)):
+				raise TypeError("max_dist must be an integer or a float")
+
+			self.range = max_dist
+
+		super().__init__(x=x, y=y)
