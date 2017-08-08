2732a04c778f9014538ec4636d9652f0c12be58f
Elan van Biljon <elan@vanbiljon.co.za>
2017-08-08 09:22:08 +0200

    Make tests more robust

--- python/tests.py
+++ python/tests.py
@@ -3,16 +3,20 @@ from coord import Coord
 
 class CoordTestCase(unittest.TestCase):
 	def setUp(self):
-		self.coord = Coord(x=1, y=2)
+		self.test_x = 1
+		self.test_y = 2
+		self.coord = Coord(x=self.test_x, y=self.test_y)
 
 	def tearDown(self):
 		del self.coord
 
 	def test_x_coord(self):
-		self.assertEqual(1, self.coord.x)
+		self.assertEqual(self.test_x, self.coord.x)
+		self.assertNotEqual(self.coord.x-1, self.coord.x)
 
 	def test_y_coord(self):
-		self.assertEqual(2, self.coord.y)
+		self.assertEqual(self.test_y, self.coord.y)
+		self.assertNotEqual(self.coord.y-1, self.coord.y)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
