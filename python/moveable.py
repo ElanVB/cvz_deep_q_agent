1ce6443bf24fb6d4a1ba91fcd679f3d3698726c6
Elan van Biljon <elan@vanbiljon.co.za>
2017-08-10 11:47:53 +0200

    Add copy and __repr__ functions Moveable

--- python/moveable.py
+++ python/moveable.py
@@ -2,10 +2,16 @@ from coord import Coord
 
 class Moveable(Coord):
 	def __init__(self, x, y, max_dist=None):
-		if max_dist != None:
-			if not isinstance(max_dist, (int, float)):
-				raise TypeError("max_dist must be an integer or a float")
+		if max_dist != None and not isinstance(max_dist, (int, float)):
+			raise TypeError("max_dist must be an integer or a float")
 
-			self.range = max_dist
-
+		self.range = max_dist
 		super().__init__(x=x, y=y)
+
+	def copy(self):
+		return Moveable(x=self.x, y=self.y, max_dist=self.range)
+
+	def __repr__(self):
+		string = super().__repr__()
+		string += " - range: {}".format(self.range) if self.range != None else ""
+		return string
