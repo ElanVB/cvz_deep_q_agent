f822fef1926da6c896ea2993a91bb236ded86463
Elan van Biljon <elan@vanbiljon.co.za>
2017-08-11 08:22:12 +0200

    Add basic functions Entity

--- python/entity.py
+++ python/entity.py
@@ -8,3 +8,11 @@ class Entity(Moveable):
 
 		self.interact_range = interact_range
 		super().__init__(x=x, y=y, max_dist=max_dist)
+
+	def copy(self):
+		return Entity(x=self.x, y=self.y,
+			interact_range=self.interact_range, max_dist=self.range)
+
+	def __repr__(self):
+		return super().__repr__() +\
+			", interact_range: {}".format(self.interact_range)
