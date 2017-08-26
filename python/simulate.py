99f5bf96333b2c4e57520a908254fb123ed8b8e1
Elan van Biljon <elan@vanbiljon.co.za>
2017-08-26 12:18:07 +0200

    Fiddle with the parameters a bit

--- python/simulate.py
+++ python/simulate.py
@@ -3,21 +3,25 @@ from random import randrange
 from environment import Environment
 from renderer import Renderer
 
-RENDER = False
-TIME_DELAY = 0.05
-MAX_HUMANS = 100
-MAX_ZOMBIES	= 100
+RENDER = True
+TIME_DELAY = 0.03
+MAX_HUMANS = 5
+MAX_ZOMBIES	= 5
 
 if RENDER:
 	render = Renderer()
 
 total_score = 0.0
-trials = 100000
+trials = 10
 for _ in range(trials):
-	if _ % int(trials/1000) == 0:
+	if _ % max(int(trials/1000), 1) == 0:
 		sys.stdout.write("\r{:.2f}% complete".format(_ * 100.0/trials))
 
-	env = Environment(randrange(1, MAX_HUMANS), randrange(1, MAX_ZOMBIES))
+	env = Environment(
+		randrange(1, MAX_HUMANS+1),
+		randrange(1, MAX_ZOMBIES+1),
+		better_rewards=True
+	)
 	while len(env.humans) > 0 and len(env.zombies) > 0:
 		x = randrange(config.WIDTH)
 		y = randrange(config.HEIGHT)
