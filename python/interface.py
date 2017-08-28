83c4eb1df5edc24b6e06370c9864abf6da2aab51
Elan van Biljon <elan@vanbiljon.co.za>
2017-08-28 11:03:22 +0200

    Create agent environment interface

---
+++ python/interface.py
@@ -0,0 +1,27 @@
+import sys, time, hyperparams
+import numpy as np
+from environment import Environment
+from renderer import Renderer
+from keras_deep_q_agent import Agent
+
+class Interface:
+	def __init__(
+		self, render=False, render_delay=0.03,
+		max_humans=1, max_zombies=1, randomness=False,
+		fine_tune=False, actions="default"
+	):
+		self._render = render
+		if render:
+			self._renderer = Renderer()
+			self._render_delay = render_delay
+
+		self._input_dim = 2 + 2*max_humans + 2*max_zombies
+
+		if actions == "default":
+			self._points = [(0, 0), (16000, 0), (0, 9000), (16000, 9000)]
+			self._output_dim = len(self._points+1) # plus one for no-op
+		else:
+			raise ValueError("action value not supported")
+
+		self._randomness = randomness
+		self._fine_tune = fine_tune
