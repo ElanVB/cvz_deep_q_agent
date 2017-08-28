4aa5bc758365c2208144f0a6d4e832598668dd03
Elan van Biljon <elan@vanbiljon.co.za>
2017-08-28 08:59:39 +0200

    Test agent strategies

--- python/simulate.py
+++ python/simulate.py
@@ -3,16 +3,16 @@ from random import randrange
 from environment import Environment
 from renderer import Renderer
 
-RENDER = True
+RENDER = False
 TIME_DELAY = 0.03
-MAX_HUMANS = 5
-MAX_ZOMBIES	= 5
+MAX_HUMANS = 1
+MAX_ZOMBIES	= 1
 
 if RENDER:
 	render = Renderer()
 
 total_score = 0.0
-trials = 10
+trials = 100000
 for _ in range(trials):
 	if _ % max(int(trials/1000), 1) == 0:
 		sys.stdout.write("\r{:.2f}% complete".format(_ * 100.0/trials))
@@ -23,8 +23,30 @@ for _ in range(trials):
 		better_rewards=True
 	)
 	while len(env.humans) > 0 and len(env.zombies) > 0:
-		x = randrange(config.WIDTH)
-		y = randrange(config.HEIGHT)
+		# x = randrange(config.WIDTH)
+		# y = randrange(config.HEIGHT)
+
+		# x = env.zombies[0].x
+		# y = env.zombies[0].y
+
+		# x = env.humans[0].x
+		# y = env.humans[0].y
+
+		points = [(0, 0), (16000, 0), (0, 9000), (16000, 9000), (env.shooter.x, env.shooter.y)]
+		min_dist = 16000+9000
+		index = -1
+		for i in range(len(points)):
+			dummy_shooter = env.shooter.copy()
+			dummy_shooter.move(points[i][0], points[i][1])
+			dist = env.humans[0].distance(dummy_shooter)
+
+			if dist < min_dist:
+				min_dist = dist
+				index = i
+
+		x = points[index][0]
+		y = points[index][1]
+
 		env.update(x, y)
 
 		if RENDER:
