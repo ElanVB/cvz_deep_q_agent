463e1ca88000a0f8ed25f8c367f3dc1637f49fa7
Elan van Biljon <elan@vanbiljon.co.za>
2017-09-20 22:55:47 +0200

    Add psuedo code

--- python/hyperparam_search.py
+++ python/hyperparam_search.py
@@ -19,3 +19,25 @@ for i in range(architecture_checks):
 
 architectures = list(set(architectures))
 architectures.sort(key=lambda x: len(x))
+
+# for each achitecture
+#	for each learning_rate
+#		create an agent, train it, store the test score
+#	for learning_rate_scans
+#		find the rates that got the best 2 scores
+#		divide the space between the rates linearly in learning_rate_segments
+#		for each learning_rate
+# 			create an agent, train it, store the test score
+# print the best leraning_rate and score
+#### write all of this to a file as you go ####
+
+# i = Interface(
+# 	learning_rate=hyperparams.learning_rate,
+# 	state_sequence_length=hyperparams.state_sequence_length,
+# 	hidden_layers=hyperparams.hidden_layers,
+# 	max_humans=1, max_zombies=1
+# )
+# i.train_agent(config=[
+# 	"experienced_replay", "experimental_network_update_delay", "frame_skip"
+# ])
+# i.test_agent()
