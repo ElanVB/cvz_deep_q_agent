5fd521eaf5b17cfb9a76d7fb3bb7a0903f265fe4
Elan van Biljon <elan@vanbiljon.co.za>
2017-09-20 22:55:22 +0200

    Add net architecture gen

--- python/hyperparam_search.py
+++ python/hyperparam_search.py
@@ -7,3 +7,15 @@ learning_rates = [10 ** -i for i in range(1, 8)]
 max_params = 1e6
 architecture_checks = 20
 layer_sizes = (8, 16, 32, 64, 128, 256, 512, 1024)
+
+architectures = []
+for i in range(architecture_checks):
+	size = np.random.randint(1, 9)
+	architecture = []
+	for j in range(size):
+		architecture.append(np.random.choice(layer_sizes))
+	architecture.sort(reverse=True)
+	architectures.append(tuple(architecture))
+
+architectures = list(set(architectures))
+architectures.sort(key=lambda x: len(x))
